b4e361dd593ba129e217bf55e10f4da664f9d614
jahoog <35244949+jahoog@users.noreply.github.com>
2022-05-04 12:28:44 -0400

    Add Store components

---
+++ src/components/StoreList/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const StoreList = () => (<>Store List</>);
+export default StoreList;
---
+++ src/components/StoreLocator/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const StoreLocator = () => (<>Store Locator</>);
+export default StoreLocator;
