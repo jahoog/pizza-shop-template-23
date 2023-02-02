4028a46492c63ae3fbc3f706ebf4bbc7243615e1
jahoog <35244949+jahoog@users.noreply.github.com>
2023-02-02 08:53:40 -0500

    Update getrepo.sh

--- shell/getrepo.sh
+++ shell/getrepo.sh
@@ -1,4 +1,4 @@
-read -p "Enter your initials: " initials
-
-git clone https://github.com/jahoog/pizza-shop-template.git andy-pizza-shop-$initials
-
+git clone https://github.com/jahoog/pizza-shop-template-23.git andy-pizza-shop
+cd andy-pizza-shop
+REPO_URL=$(aws codecommit get-repository --repository-name workshoprepo --query 'repositoryMetadata.cloneUrlHttp' --output text)
+git remote set-url origin ${REPO_URL}
